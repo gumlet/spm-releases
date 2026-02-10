8366e949a86162068df9174bee08e1e99ce90564
Prashant Pandey <prashant.krishna52@gmail.com>
2026-02-10 19:24:28 +0530

    Release 1.0.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GumletVideoPlayer",
-            url: "https://github.com/gumlet/spm-releases/releases/download/1.0.1/GumletVideoPlayer.xcframework.zip", 
+            url: "https://github.com/gumlet/spm-releases/releases/download/1.0.2/GumletVideoPlayer.xcframework.zip", 
             checksum: "0b5ad10cbcb6e519a2cdbfd00b39ce090fa48347cac68016c6d4ebb642d256dc"
         ),
     ]
