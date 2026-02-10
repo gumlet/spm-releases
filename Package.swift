bd6368c320d6b0add117cc5d3ed1db02db78d190
Prashant Pandey <prashant.krishna52@gmail.com>
2026-02-10 18:52:33 +0530

    Update checksum for GumletVideoPlayer SDK to match new release

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "GumletVideoPlayer",
             url: "https://github.com/gumlet/spm-releases/releases/download/1.0.1/GumletVideoPlayer.xcframework.zip", 
-            checksum: "3c37453ac5d56ef8cc591a7752088b46908c98f86cd6f35cb81363300e5a318f"
+            checksum: "0b5ad10cbcb6e519a2cdbfd00b39ce090fa48347cac68016c6d4ebb642d256dc"
         ),
     ]
 )
